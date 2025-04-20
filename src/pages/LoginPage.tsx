f7fec2f4e383ad23d36e920838c931831c1b4d04
Eugene Cash <eugenecash@gmail.com>
2025-04-21 11:55:54 +1200

    refactor: Use alias and add explicit extensions to vite.config.ts

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowLeftIcon, CheckCircleIcon } from 'lucide-react';
-import { Button } from '../components/ui/button';
+import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
